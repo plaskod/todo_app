8b03103710a53c4024e22d8a03ae321c7ffd9f4d
plaskod <66804071+plaskod@users.noreply.github.com>
2022-06-14 08:05:37 +0200

    Add files via upload

---
+++ src/components/todosData.js
@@ -0,0 +1,16 @@
+
+let todosData = [
+    {
+        id: 1,
+        text: "Write a react app",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Excersise",
+        completed: true
+    }
+]
+
+export default todosData
+    
